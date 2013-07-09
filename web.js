b585cab05c275624ac9bd643f37dfc9f593144ae
Dario Albertocchi <dariox@gmail.com>
2013-07-09 03:15:08 +0000

    using readFileSync/Buffer, 2nd try

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response)
 {
   //response.send('Hello World 2!');
-  response.send(buf.toString(fs.readFileSync("index.html")));
+  response.send(fs.readFileSync("index.html").toString());
 
 });
 
